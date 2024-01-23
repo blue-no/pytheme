6080b0ee65ef62b0798c635db5e7792767dc2d69
Kota Aono <k.pg.aono@gmail.com>
2024-01-23 22:05:24 +0900

    Restructure config

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -14,35 +14,31 @@ tplus3 = (t + timedelta(minutes=3)).strftime("%H:%M")
 config_dict = {
     "themes": [
         {
-            "name": "morning",
+            "name": "theme1",
             "mode": "light",
             "wp": "C:\\Windows\\Web\\Wallpaper\\ThemeB\\img24.jpg",
-            "ask_before_applying": False,
         },
         {
-            "name": "noonbreak",
+            "name": "theme2",
             "mode": "dark",
             "wp": "C:\\Windows\\Web\\Wallpaper\\ThemeB\\img25.jpg",
-            "ask_before_applying": True,
         },
         {
-            "name": "afternoon",
+            "name": "theme3",
             "mode": "light",
             "wp": "C:\\Windows\\Web\\Wallpaper\\ThemeB\\img26.jpg",
-            "ask_before_applying": True,
         },
         {
-            "name": "evening",
+            "name": "theme4",
             "mode": "dark",
             "wp": "C:\\Windows\\Web\\Wallpaper\\ThemeB\\img27.jpg",
-            "ask_before_applying": True,
         },
     ],
     "schedule": [
-        {"time": tmnus1, "theme": "morning"},
-        {"time": tplus1, "theme": "noonbreak"},
-        {"time": tplus2, "theme": "afternoon"},
-        {"time": tplus3, "theme": "evening"},
+        {"time": tmnus1, "theme": "theme1", "confirm": False},
+        {"time": tplus1, "theme": "theme2", "confirm": True},
+        {"time": tplus2, "theme": "theme3", "confirm": False},
+        {"time": tplus3, "theme": "theme4", "confirm": True},
     ],
     "application_delay": 3.0,
     "ask_interval_mins": 0.4,
